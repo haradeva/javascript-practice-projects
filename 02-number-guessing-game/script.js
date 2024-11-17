542b4c250af1c3cc880712698a807444864cb693
haradeva <haradev555@gmail.com>
2024-11-17 14:32:10 +0530

    Add number guessing game logic to script.js

--- 02-number-guessing-game/script.js
+++ 02-number-guessing-game/script.js
@@ -1,1 +1,32 @@
+const minimum = 1;
+const maximum = 100;
 
+const answer = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+
+// console.log(answer);
+
+let attempt = 0;
+let guess;
+let running = true;
+
+while (running) {
+  guess = window.prompt(`Guess a number between ${minimum} - ${maximum}`);
+
+  guess = Number(guess);
+
+  if (isNaN(guess) || guess < minimum || guess > maximum) {
+    window.alert("Please enter a valid Number");
+  } else {
+    attempt++;
+    if (guess > answer) {
+      window.alert(`TOO HIGH, TRY AGAIN!`);
+    } else if (guess < answer) {
+      window.alert(`TOO LOW, TRY AGAIN!`);
+    } else {
+      window.alert(
+        `FINALLY! ${answer} you got it right on ${attempt} attempts`
+      );
+      running = false;
+    }
+  }
+}
